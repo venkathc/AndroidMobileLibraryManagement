4052e078d593c186e0312c73c4247f08f9793e10
venkateshkumarhc <venkatesh.kumarhc@team.telstra.com>
2026-09-03 18:37:17 +0530

    Remove legacy web application

--- utils/constants.py
+++
@@ -1,8 +0,0 @@
-"""Application-wide constants."""
-
-READING_STATUSES = ("Unread", "Reading", "Read")
-WISHLIST_PRIORITIES = ("Low", "Medium", "High")
-WISHLIST_STATUSES = ("Planned", "Purchased", "Cancelled")
-LOAN_STATUSES = ("Lent", "Returned", "Overdue")
-ALLOWED_COVER_FORMATS = {"PNG", "JPEG", "WEBP"}
-MAX_COVER_UPLOAD_BYTES = 5 * 1024 * 1024
